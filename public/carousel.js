5e6ba0c6e484a79e9574b8bd2c7c93535dff98be
Jacob Harris <jacob@hackatoa.com>
2026-05-30 22:24:53 -0700

    feat: extract carousel to external JS file (CSP compliance)

---
+++ public/carousel.js
@@ -0,0 +1,12 @@
+(function () {
+  var imgs, idx = 0;
+  function cycle() {
+    imgs[idx].classList.remove('carousel-img--active');
+    idx = (idx + 1) % imgs.length;
+    imgs[idx].classList.add('carousel-img--active');
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    imgs = document.querySelectorAll('.avatar-carousel .carousel-img');
+    if (imgs.length > 1) setInterval(cycle, 4000);
+  });
+})();
